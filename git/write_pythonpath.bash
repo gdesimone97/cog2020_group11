0b29a8ccfba4a01e91e90062362bab2e25519da1
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2020-11-14 23:23:56 +0100

    add check print

--- git/write_pythonpath.bash
+++ git/write_pythonpath.bash
@@ -4,3 +4,4 @@ D=$(realpath pynaoqi-python2.7-2.5.7.1-linux64)
 cd ..
 echo "export PYTHONPATH=\${PYTHONPATH}:$D/lib/python2.7/site-packages" >> devel/setup.bash
 echo "export DYLD_LIBRARY_PATH=\${DYLD_LIBRARY_PATH}:$D/lib" >> devel/setup.bash
+echo "Tutto ok"
