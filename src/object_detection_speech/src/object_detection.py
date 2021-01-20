bbf706c6a9852438a3949db073a7ffd9236e68e8
peppo97 <gds2011@libero.it>
2021-01-20 05:48:50 -0800

    update obj_detection

--- src/object_detection_speech/src/object_detection.py
+++ src/object_detection_speech/src/object_detection.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     print('Loading model...', end='')
     #Create a Condition object called scheduler to manage inter-thread communication
     scheduler = Condition()
-    MODEL_NAME = "efficientdet_d2_coco17_tpu-32" #Modify model name here
+    MODEL_NAME = "centernet_resnet50_v1_fpn_512x512_coco17_tpu-8" #Modify model name here
     DET_PATH = os.path.dirname(__file__) + '/../models/' + MODEL_NAME
     #Load the tf model
     start = time.time()
@@ -195,6 +195,13 @@ if __name__ == '__main__':
     elapse = str(round(end - start, 2))
     print('Done!')
     #Create the Detector object
+    
+    img = np.full((512,512,3),0)
+    img = img[:, :, ::-1]
+    input_tensor = tf.convert_to_tensor(img)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    detect_fn(input_tensor)
+    
     det = Detector()
     print("Model loaded in", elapse, "seconds")
     rospy.spin()
